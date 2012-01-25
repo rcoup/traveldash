62ce1c4e90bf961cce9b23beb3a0394f42732355
Robert Coup <robert@coup.net.nz>
2012-01-26 11:13:14 +1300

    Add UserVoice widget support

--- traveldash/context_processors.py
+++ traveldash/context_processors.py
@@ -3,3 +3,7 @@ from django.conf import settings
 
 def google_analytics(request):
     return {'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY}
+
+
+def uservoice(request):
+    return {'USERVOICE_WIDGET': settings.USERVOICE_WIDGET}
